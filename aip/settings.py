72c4161d874be243e5b30685c3690bea39f1cd1e
prokashmax <prokashpul2@gmail.com>
2021-01-19 23:44:13 +0600

    first commit

--- aip/settings.py
+++ aip/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest-framework',
-    'rest-framework-authtoken',
-    'profiles_aip',
+    'profiles_api',
 ]
 
 MIDDLEWARE = [
