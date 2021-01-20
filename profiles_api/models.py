62505d43911505f36b28fd87dd43f3822f3a6351
prokashmax <prokashpul2@gmail.com>
2021-01-20 23:01:04 +0600

    change profile_api models file and settings

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -4,29 +4,30 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 class UserProfileManager(BaseUserManager):
-    """ Manger User Profile """
+    """Manager for user profiles"""
+
     def create_user(self, email, name, password=None):
-        """Create  for user profile """
+        """Create a new user profile"""
         if not email:
-            raise ValueError('User Must Have an email address')
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
-    def create_supperuser(self, email, name, password):
-        """create and save New  supperuser give with details"""
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
 
-        user.is_supperuser = True
+        user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
 
         return user
-
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
     """ Database Models for users in the system """
