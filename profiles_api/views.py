994c4f6724fe6d1c001dd7cb4f48a949c9646f16
prokashmax <prokashpul2@gmail.com>
2021-01-21 00:20:50 +0600

    creat serializet form and status setup

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 class HelloApiView(APIView):
     """Test api View """
+    serializer_class = serializers.HelloApiSerializer
     def get(self, request, format=None):
         """Return a list of api formate"""
         an_apiview = [
@@ -13,4 +16,21 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'Hello!','an_apiview':an_apiview})
+
+
+    def post(self, request):
+        """ Create a hello massage with our name """
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+
 
